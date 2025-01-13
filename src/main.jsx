8d3b36a098289cb494b6536b1b998f6481085dd5
NeelanshaAgarwal <na1027@srmist.edu.in>
2025-01-13 19:25:11 +0530

    fix: refactor main.jsx to streamline ReactDOM rendering and improve code consistency

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
-import React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-} else {
-  console.error("Could not find element with id 'root'");
-}
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
